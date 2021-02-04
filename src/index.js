19ece63a82f95b6de85e47a1d295eff50a398ca9
YaroslavKH1990 <yaroslav.kh.ua@gmail.com>
2021-02-04 19:54:52 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './styles.css';
+import './base.scss';
+
+import './helpers/normalize.scss';
+import './helpers/variables.scss';
+import './helpers/placeholders.scss';
+
+import './components/_fonts.scss';
+import './components/_header.scss';
+import './components/_banner.scss';
+import './components/_about-section.scss';
+import './components/_friends-section.scss';
+import './components/_help-section.scss';
+import './components/_donation-section.scss';
+import './components/_footer-section.scss';
